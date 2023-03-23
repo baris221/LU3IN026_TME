dd5ca8df64173a99c491d3ad9763f27165226ca6
unknown <bariskaftancioglu@gmail.com>
2023-03-23 14:33:22 +0100

    Fin TME6

--- iads/evaluation.py
+++ iads/evaluation.py
@@ -12,7 +12,20 @@ Année: LU3IN026 - semestre 2 - 2022-2023, Sorbonne Université
 # import externe
 import numpy as np
 import pandas as pd
+import math
 
 # ------------------------ 
 #TODO: à compléter  plus tard
 # ------------------------ 
+def analyse_perfs(L):
+    """ L : liste de nombres réels non vide
+        rend le tuple (moyenne, écart-type)
+    """
+    moyenne=sum(L)/len(L)
+    
+    ecart_type=0
+    for pref in L:
+        ecart_type=ecart_type+((pref-moyenne)*(pref-moyenne))
+        
+    return (moyenne,math.sqrt(ecart_type/len(L)))
+    raise NotImplementedError("Vous devez implémenter cette fonction !")    
