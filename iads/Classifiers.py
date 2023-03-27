cf57f9199edb2b4ab66556090469f9fe8af2c506
unknown <bariskaftancioglu@gmail.com>
2023-03-27 23:12:51 +0200

    TME 7 done

--- iads/Classifiers.py
+++ iads/Classifiers.py
@@ -12,6 +12,8 @@ Année: LU3IN026 - semestre 2 - 2022-2023, Sorbonne Université
 # Import de packages externes
 import numpy as np
 import pandas as pd
+import math
+import graphviz as gv
 
 # ---------------------------
 class Classifier:
@@ -113,7 +115,45 @@ class ClassifierKNN(Classifier):
         self.label = label_set
         #raise NotImplementedError("Please Implement this method")
 
+class ClassifierKNN_MC(Classifier):
 
+    # ATTENTION : il faut compléter cette classe avant de l'utiliser !
+    
+    def __init__(self, input_dimension, k):
+        self.input_dimension = input_dimension
+        self.k = k
+        self.ref_desc = None
+        self.ref_label = None
+        
+    def score(self,x):
+        """ 
+        Enregistrer les étiquettes des k points les plus proches du point prédit et retourner le dictionnaire
+        """
+        ref_tab = [np.linalg.norm(self.ref_desc[i]-x) for i in range(len(self.ref_label))]
+        ind=np.argsort(np.asarray(ref_tab),axis=0)
+        
+        d = dict()
+        for i in range(self.k):
+            if self.ref_label[ind[i]] not in d:
+                d[self.ref_label[ind[i]]] = 1
+            else:
+                d[self.ref_label[ind[i]]] += 1
+        return d   
+    
+    def predict(self, x):
+        d = self.score(x)
+        return max(d, key=lambda k: d[k])
+
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        self.ref_desc = desc_set
+        self.ref_label = label_set
+        
+        
 class ClassifierPerceptron(Classifier):
     """ Perceptron de Rosenblatt
     """
@@ -240,3 +280,250 @@ class ClassifierPerceptronBiais(ClassifierPerceptron):
                 self.w += self.learning_rate*np.dot(Xi,Yi)
                 self.allw.append(self.w.copy())
         #raise NotImplementedError("Vous devez implémenter cette méthode !")
+        
+def classe_majoritaire(Y):
+    """ Y : (array) : array de labels
+        rend la classe majoritaire ()
+    """
+    #### A compléter pour répondre à la question posée
+    valeurs, index_list, nb_fois = np.unique(Y,return_index = True,return_counts=True)
+    a = np.unique(nb_fois)
+    
+    #Si il y a une seule valeur, on le renvoit
+    if len(a)==1:
+        res = Y[np.sort(index_list)[0]]
+        return res
+    else:
+        i = np.argmax(nb_fois)
+        return valeurs[i]
+    
+    
+def shannon(P):
+    """ list[Number] -> float
+        Hypothèse: la somme des nombres de P vaut 1
+        P correspond à une distribution de probabilité
+        rend la valeur de l'entropie de Shannon correspondante
+        rem: la fonction utilise le log dont la base correspond à la taille de P
+    """
+    k = len(P)
+    if k<=1:
+        return 0.
+    entropie_Shannon = 0
+    for pi in P:
+        if pi>0:
+            entropie_Shannon =entropie_Shannon- pi*math.log(pi,k)   
+    return entropie_Shannon
+
+def entropie(Y):
+    """ Y : (array) : ensemble de labels de classe
+        retourne l'entropie en utilisant la formule Shannon de l'ensemble Y
+    """
+    classes, nb_fois = np.unique(Y, return_counts=True)
+    P = []
+    for i in range(len(classes)):
+        P.append(nb_fois[i]/sum(nb_fois))
+    return shannon(P)
+
+class NoeudCategoriel:
+    """ Classe pour représenter des noeuds d'un arbre de décision
+    """
+    def __init__(self, num_att=-1, nom=''):
+        """ Constructeur: il prend en argument
+            - num_att (int) : le numéro de l'attribut auquel il se rapporte: de 0 à ...
+              si le noeud se rapporte à la classe, le numéro est -1, on n'a pas besoin
+              de le préciser
+            - nom (str) : une chaîne de caractères donnant le nom de l'attribut si
+              il est connu (sinon, on ne met rien et le nom sera donné de façon 
+              générique: "att_Numéro")
+        """
+        self.attribut = num_att    # numéro de l'attribut
+        if (nom == ''):            # son nom si connu
+            self.nom_attribut = 'att_'+str(num_att)
+        else:
+            self.nom_attribut = nom 
+        self.Les_fils = None       # aucun fils à la création, ils seront ajoutés
+        self.classe   = None       # valeur de la classe si c'est une feuille
+        
+    def est_feuille(self):
+        """ rend True si l'arbre est une feuille 
+            c'est une feuille s'il n'a aucun fils
+        """
+        return self.Les_fils == None
+    
+    def ajoute_fils(self, valeur, Fils):
+        """ valeur : valeur de l'attribut de ce noeud qui doit être associée à Fils
+                     le type de cette valeur dépend de la base
+            Fils (NoeudCategoriel) : un nouveau fils pour ce noeud
+            Les fils sont stockés sous la forme d'un dictionnaire:
+            Dictionnaire {valeur_attribut : NoeudCategoriel}
+        """
+        if self.Les_fils == None:
+            self.Les_fils = dict()
+        self.Les_fils[valeur] = Fils
+        # Rem: attention, on ne fait aucun contrôle, la nouvelle association peut
+        # écraser une association existante.
+    
+    def ajoute_feuille(self,classe):
+        """ classe: valeur de la classe
+            Ce noeud devient un noeud feuille
+        """
+        self.classe    = classe
+        self.Les_fils  = None   # normalement, pas obligatoire ici, c'est pour être sûr
+        
+    def classifie(self, exemple):
+        """ exemple : numpy.array
+            rend la classe de l'exemple (pour nous, soit +1, soit -1 en général)
+            on rend la valeur 0 si l'exemple ne peut pas être classé (cf. les questions
+            posées en fin de ce notebook)
+        """
+        if self.est_feuille():
+            return self.classe
+        if exemple[self.attribut] in self.Les_fils:
+            # descente récursive dans le noeud associé à la valeur de l'attribut
+            # pour cet exemple:
+            return self.Les_fils[exemple[self.attribut]].classifie(exemple)
+        else:
+            # Cas particulier : on ne trouve pas la valeur de l'exemple dans la liste des
+            # fils du noeud... Voir la fin de ce notebook pour essayer de résoudre ce mystère...
+            print('\t*** Warning: attribut ',self.nom_attribut,' -> Valeur inconnue: ',exemple[self.attribut])
+            return 0
+    
+    def to_graph(self, g, prefixe='A'):
+        """ construit une représentation de l'arbre pour pouvoir l'afficher graphiquement
+            Cette fonction ne nous intéressera pas plus que ça, elle ne sera donc pas expliquée            
+        """
+        if self.est_feuille():
+            g.node(prefixe,str(self.classe),shape='box')
+        else:
+            g.node(prefixe, self.nom_attribut)
+            i =0
+            for (valeur, sous_arbre) in self.Les_fils.items():
+                sous_arbre.to_graph(g,prefixe+str(i))
+                g.edge(prefixe,prefixe+str(i), valeur)
+                i = i+1        
+        return g
+    
+    
+def construit_AD(X,Y,epsilon,LNoms = []):
+    """ X,Y : dataset
+        epsilon : seuil d'entropie pour le critère d'arrêt 
+        LNoms : liste des noms de features (colonnes) de description 
+    """
+    
+    entropie_ens = entropie(Y)
+    if (entropie_ens <= epsilon):
+        # ARRET : on crée une feuille
+        noeud = NoeudCategoriel(-1,"Label")
+        noeud.ajoute_feuille(classe_majoritaire(Y))
+    else:
+        min_entropie = 1.1
+        gain_max = sys.float_info.min
+        i_best = -1
+        Xbest_valeurs = None
+        
+        #############
+        
+        # COMPLETER CETTE PARTIE : ELLE DOIT PERMETTRE D'OBTENIR DANS
+        # i_best : le numéro de l'attribut qui minimise l'entropie
+        # min_entropie : la valeur de l'entropie minimale
+        # Xbest_valeurs : la liste des valeurs que peut prendre l'attribut i_best
+        #
+        # Il est donc nécessaire ici de parcourir tous les attributs et de calculer
+        # la valeur de l'entropie de la classe pour chaque attribut.
+        entropie_classe = entropie(Y)
+        for attribut in LNoms:
+            index = LNoms.index(attribut) # indice de l'attribut dans LNoms
+            attribut_valeurs = np.unique([x[index] for x in X]) #liste des valeurs (sans doublon) prises par l'attribut
+            # Liste des entropies de chaque valeur pour l'attribut courant
+            entropies = []
+            # Liste des probabilités de chaque valeur pour l'attribut courant
+            probas_val = []
+            
+            for v in attribut_valeurs:
+                # on construit l'ensemble des exemples de X qui possède la valeur v ainsi que l'ensemble de leurs labels
+                X_v = [i for i in range(len(X)) if X[i][index] == v]
+                Y_v = np.array([Y[i] for i in X_v])
+                e_v = entropie(Y_v)
+                entropies.append(e_v)
+                probas_val.append(len(X_v)/len(X))
+            
+            entropie_cond = 0
+            
+            for i in range(len(attribut_valeurs)):
+                entropie_cond += probas_val[i]*entropies[i]
+                
+            Is = entropie_classe - entropie_cond
+            
+            if Is>gain_max:
+                gain_max = Is
+                i_best = index
+                Xbest_valeurs = attribut_valeurs
+        
+        
+        ############
+        
+        if len(LNoms)>0:  # si on a des noms de features
+            noeud = NoeudCategoriel(i_best,LNoms[i_best])    
+        else:
+            noeud = NoeudCategoriel(i_best)
+        for v in Xbest_valeurs:
+            noeud.ajoute_fils(v,construit_AD(X[X[:,i_best]==v], Y[X[:,i_best]==v],epsilon,LNoms))
+    return noeud
+
+class ClassifierArbreDecision(Classifier):
+    """ Classe pour représenter un classifieur par arbre de décision
+    """
+    
+    def __init__(self, input_dimension, epsilon, LNoms=[]):
+        """ Constructeur
+            Argument:
+                - intput_dimension (int) : dimension de la description des exemples
+                - epsilon (float) : paramètre de l'algorithme (cf. explications précédentes)
+                - LNoms : Liste des noms de dimensions (si connues)
+            Hypothèse : input_dimension > 0
+        """
+        self.dimension = input_dimension
+        self.epsilon = epsilon
+        self.LNoms = LNoms
+        # l'arbre est manipulé par sa racine qui sera un Noeud
+        self.racine = None
+        
+    def toString(self):
+        """  -> str
+            rend le nom du classifieur avec ses paramètres
+        """
+        return 'ClassifierArbreDecision ['+str(self.dimension) + '] eps='+str(self.epsilon)
+        
+    def train(self, desc_set, label_set):
+        """ Permet d'entrainer le modele sur l'ensemble donné
+            desc_set: ndarray avec des descriptions
+            label_set: ndarray avec les labels correspondants
+            Hypothèse: desc_set et label_set ont le même nombre de lignes
+        """        
+        ##################
+        ## COMPLETER ICI !
+        ##################
+        self.racine = construit_AD(desc_set,label_set,self.epsilon,self.LNoms)
+    
+    def score(self,x):
+        """ rend le score de prédiction sur x (valeur réelle)
+            x: une description
+        """
+        # cette méthode ne fait rien dans notre implémentation :
+        pass
+    
+    def predict(self, x):
+        """ x (array): une description d'exemple
+            rend la prediction sur x             
+        """
+        ##################
+        ## COMPLETER ICI !
+        ##################
+        classe = self.racine.classifie(x)
+        return classe
+
+    def affiche(self,GTree):
+        """ affichage de l'arbre sous forme graphique
+            Cette fonction modifie GTree par effet de bord
+        """
+        self.racine.to_graph(GTree)
--- iads/evaluation.py
+++ iads/evaluation.py
@@ -13,6 +13,7 @@ Année: LU3IN026 - semestre 2 - 2022-2023, Sorbonne Université
 import numpy as np
 import pandas as pd
 import math
+import copy
 
 # ------------------------ 
 #TODO: à compléter  plus tard
@@ -29,3 +30,61 @@ def analyse_perfs(L):
         
     return (moyenne,math.sqrt(ecart_type/len(L)))
     raise NotImplementedError("Vous devez implémenter cette fonction !")    
+    
+def crossval_strat(X,Y,n_iterations,iteration):
+    
+    unique_Y=np.unique(Y)
+    index_list=[]
+    for i in range(len(unique_Y)):
+        indexs, = np.where(Y == unique_Y[i])
+        index_list.append(indexs)
+    
+    index_test_list=[]
+    for i in range(len(index_list)):
+        index_i=index_list[i]
+        indexs_test=index_i[iteration*(len(index_i) // n_iterations): (iteration+1)*len(index_i)//n_iterations]
+        index_test_list.append(indexs_test)
+
+    X_Test=X[index_test_list[0]]
+    for i in range(1,len(index_test_list)):
+        X_Test=np.concatenate((X_Test,X[index_test_list[i]]))
+     
+    Y_Test=Y[index_test_list[0]]
+    for i in range(1,len(index_test_list)):
+        Y_Test=np.concatenate((Y_Test,Y[index_test_list[i]]))
+    
+    index_train=[]
+    for i in range(len(X)):
+        is_in=False
+        for index in index_test_list:
+            if i in index:
+                is_in=True
+    
+        if is_in==False:
+            index_train.append(i)
+    
+    X_Train=X[index_train]
+    Y_Train=Y[index_train]
+    
+    return X_Train,Y_Train,X_Test,Y_Test
+
+def validation_croisee(C, DS, nb_iter):
+    """ Classifieur * tuple[array, array] * int -> tuple[ list[float], float, float]
+    """
+    X, Y = DS   
+    perf = []
+    
+    for i in range(nb_iter):
+        newC = copy.deepcopy(C)
+        desc_train,label_train,desc_test,label_test=crossval_strat(X,Y,nb_iter,i)
+        newC.train(desc_train,label_train)
+        acc_i=newC.accuracy(desc_test,label_test)
+        perf.append(acc_i)
+        print(i,": taille app.= ",label_train.shape[0],"taille test= ",label_test.shape[0],"Accuracy:",acc_i)
+    
+    ########################## COMPLETER ICI 
+    
+    
+    ##########################
+    (perf_moy, perf_sd) = analyse_perfs(perf)
+    return (perf, perf_moy, perf_sd)
